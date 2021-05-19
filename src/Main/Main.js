61dfc4d80d9ba55cacfeab0a7783caa1b24959b9
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-05-19 16:58:31 -0400

    add more landing page content, update arrow, add animation to arrow

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -9,14 +9,14 @@ function Main() {
         window.onscroll = function() {scrollFunction()};
         
         function scrollFunction() {
-        const header = document.getElementById("Header");
+        // const header = document.getElementById("Header");
         const arrow = document.getElementById("arrow");
 
           if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            header.classList.add("sticky");
+            // header.classList.add("sticky");
             arrow.classList.add("hidden");
           } else {
-            header.classList.remove("sticky");
+            // header.classList.remove("sticky");
             arrow.classList.remove("hidden");
           }
         }
@@ -31,13 +31,28 @@ function Main() {
   return (
     <>
     <Header />
-    <div className="Main">
-      <div className="Main-header">
-        <p>Welcome</p>
+    <div className="Landing">
+      <div className="Landing-group">
+        <div className="Landing-header">
+          <img src="./logo.png" alt="letter b logo" className="landing-logo" />
+          <p className="name">ryana Kitchen</p>
+        </div>
+        <p className="title">Full Stack Software Engineer | Organization Aficionado</p>
+
         <div className="arrow" id="arrow">
           <img src="./arrow.png" alt="down arrow" />
         </div>
       </div>
+    </div>
+    
+    <div className="about">
+      <div >
+        <h3>Hi, I'm Bryana. It's nice to meet you!</h3>
+        <p>
+          I'm a full stack software engineer who loves making environments more organized.
+        </p>
+      </div>
+
         <div id="projects">Projects</div>
         <div id="mission">Mission</div>
         <div id="contact">Mission</div>
