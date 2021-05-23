f7b85a141ab37a3b8970ce4bbb5bb1fcb6f52bfa
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-05-23 13:27:51 -0400

    fix/update about content

--- src/About/About.js
+++ src/About/About.js
@@ -9,7 +9,7 @@ export default function About() {
                 I'm a full stack software engineer who loves making environments more organized and efficient.  With a background in logistics, I apply skills I've gained as an account manager by creating applications that are scalable, while maintaining quality. I appreciate and enjoy seeing the flow of data of a full stack application beginning with raw data in a database that makes its way to a well thought out front end application.  
             </p>
             <p>
-                When I'm not coding, I enjoy being outside, hiking, traveling and exploring, or simp coffee and taking in the scenery.
+                When I'm not coding, I enjoy being outside, hiking, traveling and exploring, and enjoying the simple pleasures in life.
             </p>
         </div>
     )
