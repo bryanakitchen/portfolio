f028b0714ff419d477f4653ad7caa17b1025581d
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-05-24 14:16:47 -0400

    left align contacts and fine tune mission

--- src/Mission/Mission.js
+++ src/Mission/Mission.js
@@ -7,10 +7,10 @@ export default function Mission() {
             <div className="inner">
                 <p>Mission</p>
                 <p>
-                    My mission is to live my life with integrity, compassion, and excellence and build tangible and meaningful solutions to human-centered problems.
+                    My mission is to live my life with integrity, compassion, and excellence, building tangible and meaningful solutions to human-centered problems.
                 </p>
                 <p>
-                    I will do this by using mistakes as opportunities, practicing and encouraging inclusivity, and maintaining a mindset of continous improvement.
+                    I use mistakes as opportunities, practice and encourage inclusivity, and maintain a mindset of continous improvement.
                 </p>
             </div>
         </div>
